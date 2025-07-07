ad38ec5efde5ca3732a866831c9b70a60fc1e602
Dhanapalagiri22 <palagiridhanalakshmi2207@gmail.com>
2025-07-07 13:10:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -320,4 +320,4 @@ def bookanappointment():
 # Run the app
 # ----------------------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
